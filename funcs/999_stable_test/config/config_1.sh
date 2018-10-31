2759e42c002acf13ccb8aa3d22f14babcbe457a9
[ivankabestwill] <[278467214@qq.com]>
2018-10-31 10:20:01 +0800

    for pcap_save test. storage file number no need 1024.

--- funcs/999_stable_test/config/config_1.sh
+++ funcs/999_stable_test/config/config_1.sh
@@ -4,6 +4,6 @@ RESULT=$3
 INT=$4
 FUNC_PATH=$5
 
-# cp -fr ${FUNC_PATH}/config_files/etc_1 ${INSTALL_PATH}/nta/etc
+cp -fr ${FUNC_PATH}/config_files/pcap_save.ini ${INSTALL_PATH}/nta/etc/pcap_save/pcap_save.ini
 # ${INSTALL_PATH}/nta/bin/nta_start.sh restart
 ${INSTALL_PATH}/nta/bin/pcap_save.sh restart >/dev/null 2>/dev/null
