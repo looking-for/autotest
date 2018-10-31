778561c4c3483d1958a3b8dc96e08d208d272ba0
[ivankabestwill] <[278467214@qq.com]>
2018-10-31 10:21:44 +0800

    del storage file for post

--- funcs/999_stable_test/post/post_1.sh
+++ funcs/999_stable_test/post/post_1.sh
@@ -7,3 +7,5 @@ FUNC_PATH=$5
 # cp -fr ${FUNC_PATH}/config_files/etc_1 ${INSTALL_PATH}/nta/etc
 # ${INSTALL_PATH}/nta/bin/nta_start.sh restart
 ${INSTALL_PATH}/nta/bin/pcap_save.sh stop >/dev/null 2>/dev/null
+sleep 3
+rm -fr /tmp/nta/pcapsavefile >/dev/null 2>/dev/null
